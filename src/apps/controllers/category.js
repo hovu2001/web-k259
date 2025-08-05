0c8ab70a80f9b050db3f584a27d84372a156af28
hovu2001 <nguyenduyhoang26042002@gmail.com>
2025-08-05 21:54:05 +0700

    node 09

--- src/apps/controllers/category.js
+++ src/apps/controllers/category.js
@@ -1,5 +1,8 @@
-exports.index = (req,res) => {
-    return res.send("Get all category");
+const CategoryModel = require("../models/category");
+exports.index = async (req,res) => {
+    const categories = await CategoryModel.find()
+    .sort({ _id: -1 });
+    return res.render("admin/categories/category", { categories });
 };
 exports.create = (req,res) => {
     return res.send("Create category");
--- src/apps/controllers/user.js
+++ src/apps/controllers/user.js
@@ -1,5 +1,10 @@
-exports.index = (req,res) => {
-    return res.send("Get all user");
+const UserModel = require("../models/user");
+exports.index = async (req, res) => {
+  
+    const users = await UserModel.find()
+    .sort({ _id: -1 });
+    return res.render("admin/users/user", { users });
+   
 };
 exports.create = (req,res) => {
     return res.send("Create user");
