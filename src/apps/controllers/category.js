e6dc1d7e12e869f414a6d2106c13b0525673a312
hovu2001 <nguyenduyhoang26042002@gmail.com>
2025-08-06 20:09:54 +0700

    node 10

--- src/apps/controllers/category.js
+++ src/apps/controllers/category.js
@@ -1,8 +1,25 @@
 const CategoryModel = require("../models/category");
+const paginate = require("../../common/paginate");
 exports.index = async (req,res) => {
+
+    const page = Number(req.query.page) || 1;
+    const limit = 3;
+    const skip = page * limit - limit;
+    const totalRows = await CategoryModel.find().countDocuments();
+    const totalPages = Math.ceil(totalRows / limit);
+
     const categories = await CategoryModel.find()
-    .sort({ _id: -1 });
-    return res.render("admin/categories/category", { categories });
+    .sort({ _id: -1 })
+    .skip(skip)
+    .limit(limit);
+    return res.render("admin/categories/category", { 
+        categories,
+        paginate: paginate(totalPages, limit, page),
+        prev: page - 1,
+        next: page + 1,
+        page,
+        totalPages,
+     });
 };
 exports.create = (req,res) => {
     return res.send("Create category");
--- src/apps/controllers/product.js
+++ src/apps/controllers/product.js
@@ -8,6 +8,7 @@ exports.index = async (req, res) => {
   const page = Number(req.query.page) || 1;
   const limit = 10;
   const skip = page * limit - limit;
+  
   const totalRows = await ProductModel.find().countDocuments();
   const totalPages = Math.ceil(totalRows / limit);
   const products = await ProductModel.find()
